0db87b429310fd56d41f5b2298b3007cc8ebf6fc
kulkarni-sonali <sonali.kulkarni2022@gmail.com>
2026-02-20 15:25:26 +0530

    File_ handling initial codes

--- Learn_Module.py
+++ Learn_Module.py
@@ -59,7 +59,7 @@ print("Shuffling number list: ")
 random.shuffle(list1)
 print(list1)
 
-#list2 = ['A','B,'C','D','E']
+#list2 = ['A','B','C','D','E']
        # print("Shuffling character list: ")
 #random.shuffle(list2)
 #print(list2)
@@ -80,3 +80,94 @@ print(random.sample(mylist,k=3))    # ['Pineapple', 'Apple', 'Banana'] any 3 ite
 # random.unifrom(20,50) any 2 numbers
 
 print(random.uniform(20,50))    #26.159249403393105 any random decimal or whole number
+
+
+# * BUILT IN MODULE ( DETERMINE MODULE)
+# * It deals with date, times and time intervals. date and datetime in python are the objects, 
+# so when you manipulatethem, you areb actually manipulating objects and not string or timestamps. 
+# * Whenever you manipulate dates or time, you need to import datetime function.
+
+# Example 1 - To get current date 
+
+import datetime
+ob=datetime.date.today()
+print(ob)
+
+# Example 1 - To get current date and time
+import datetime
+
+#ob=datetime.datetime.date.now()   # This function has been discontinued
+#print(ob)
+ob=datetime.datetime.now().date()
+print(ob)
+ob=datetime.datetime.now()
+print(ob)
+
+# DATE CLASS  # Date in the format of YYYY-MM-DD
+# SYNTAX - classdatetime.date(year, month ,day)
+
+from datetime import date
+
+Today=date.today()
+
+print("Current date =", Today)
+print("Current Year =", Today.year)
+print("Current Day = ", Today.day)
+
+# TIME CLASS
+from datetime import time
+
+time(hour=0, minute=0, second=0)
+a = time()
+print("a= ",a)
+
+#time(hour, minute and second)
+b=time(15,30,56)
+print("b=",b)
+print("Hour= ",b.hour)
+print("Minute = ",b.minute)
+print("second =", b.second)
+print("Microsecond= ",b.microsecond)
+
+#time(hour,minute and second)
+c=time(hour=15, minute=30, second=56)
+print("c=",c)
+
+#time(hour,minute, second, microsecond)
+d=time(15,30,56,234566)
+print("d=",d)
+
+#DATETIME CLASS
+
+from datetime import datetime
+today=datetime.now()
+print("current Date and Time is ", today)
+
+#datetime(year,month,day)
+a=datetime(2021,2,6)
+print(a)
+
+#datetime(year,month,day,hour,minute,second,microsecond)
+b=datetime(2021,2,6,15,30,56,342380)
+print(b)
+
+# TIMEDATE CLASS
+# Difference between two dates and times
+
+D1=date.today()
+D2=date(year=2004, month=3,day=30)
+
+D3=D1-D2
+print("D3=",D3)
+
+T1=datetime(year=2001,month=2,day=6,hour=7,minute =9, second=33)
+T2=datetime(year=2020, month=12,day=10,hour=5,minute=55,second=13)
+T3=T1-T2
+print("T3= ",T3)
+T4=T2-T1
+print("T4=",T4)
+
+
+
+
+
